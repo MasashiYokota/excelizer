8aaa6e2aab3dd97b3a365069fae160c5cbb8504c
Masashi Yokota <taf_masashi@me.com>
2020-05-30 04:51:56 +0900

    Add astyle linter for c language

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 all:
 	cd go_src && make excelizir_nif.so && cd .. && cp go_src/excelizir_nif.so priv
 
+format:
+	astyle -s2 -n --style=google go_src/*.c
+
+check:
+	astyle -s2 -n --style=google --dry-run go_src/excelizir.c
+
 clean:
 	cd go_src && make clean && cd .. && rm priv/excelizir_nif.so
 
--- go_src/excelizir.c
+++ go_src/excelizir.c
@@ -6,16 +6,15 @@
 // erlang string と c string変換 https://stackoverflow.com/questions/12928943/how-do-you-return-a-string-from-an-erlang-c-node
 
 ERL_NIF_TERM read_sheet(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-	ErlNifBinary enifFilename;
-	ErlNifBinary enifSheetName;
-	enif_inspect_binary(env, argv[0], &enifFilename);
-	enif_inspect_binary(env, argv[1], &enifSheetName);
-	return ReadSheet(env, enifFilename, enifSheetName);
+  ErlNifBinary enifFilename;
+  ErlNifBinary enifSheetName;
+  enif_inspect_binary(env, argv[0], &enifFilename);
+  enif_inspect_binary(env, argv[1], &enifSheetName);
+  return ReadSheet(env, enifFilename, enifSheetName);
 }
 
-static ErlNifFunc excelixir_nif_funcs[] =
-{
-	{"read_sheet", 2, read_sheet},
+static ErlNifFunc excelixir_nif_funcs[] = {
+  {"read_sheet", 2, read_sheet},
 };
 
 ERL_NIF_INIT(Elixir.Excelizir.Base, excelixir_nif_funcs, NULL, NULL, NULL, NULL)
