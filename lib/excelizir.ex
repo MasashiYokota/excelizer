42ae4445f257bd9bd442de08b10f150b69caf555
Masashi Yokota <taf_masashi@me.com>
2020-05-30 17:46:14 +0900

    Refactor

--- lib/excelizir.ex
+++ lib/excelizir.ex
@@ -1,23 +1,2 @@
-defmodule Excelizir.Base do
-  @compile {:autoload, false}
-  @on_load {:init, 0}
-
-  def init do
-    case load_nif() do
-      :ok ->
-        :ok
-
-      e ->
-        IO.inspect(e)
-    end
-  end
-
-  def read_sheet(filename, sheetname)
-  def read_sheet(_, _), do: IO.inspect(1)
-
-  defp load_nif do
-    # path = :filename.join('./go_src/nif_excelizir')
-    path = :filename.join(:code.priv_dir(:excelizir), 'excelizir_nif')
-    :erlang.load_nif(path, 0)
-  end
+defmodule Excelizir do
 end
---
+++ lib/excelizir/base.ex
@@ -0,0 +1,26 @@
+defmodule Excelizir.Base do
+  @compile {:autoload, false}
+  @on_load {:init, 0}
+
+  def init do
+    case load_nif() do
+      :ok ->
+        :ok
+
+      _ ->
+        raise """
+        An error occurred when loading Excelizir Go library.
+        Make sure your environemnt (e.g. C compiler, Erlang version, Elixir version).
+        """
+    end
+  end
+
+  def read_sheet(filename, sheetname)
+  def read_sheet(_, _), do: :erlang.nif_error(:not_loaded)
+
+  defp load_nif do
+    # path = :filename.join('./go_src/nif_excelizir')
+    path = :filename.join(:code.priv_dir(:excelizir), 'excelizir_nif')
+    :erlang.load_nif(path, 0)
+  end
+end
