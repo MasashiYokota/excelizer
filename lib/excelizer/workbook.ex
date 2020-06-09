f5205ac517db7e048ab89dc47eb406753c711763
Masashi Yokota <taf_masashi@me.com>
2020-06-09 15:00:19 +0900

    Fix Excelizer.Workbook module

--- lib/excelizer/workbook.ex
+++ lib/excelizer/workbook.ex
@@ -4,17 +4,6 @@ defmodule Excelizer.Workbook do
   """
   alias Excelizer.Native.Base
 
-  @spec read_sheet(String.t(), String.t()) :: Base.nif_resp(Base.file_id())
-  def read_sheet(filename, sheetname), do: Base.read_sheet(filename, sheetname)
-
-  @spec read_sheet!(String.t(), String.t()) :: Base.file_id()
-  def read_sheet!(filename, sheetname) do
-    case Base.read_sheet(filename, sheetname) do
-      {:ok, resp} -> resp
-      {:error, err_msg} -> raise Excelizer.Exception, message: err_msg
-    end
-  end
-
   @spec save(Base.file_id()) :: Base.nif_resp(Base.file_id())
   def save(file_id), do: Base.save(file_id)
 
@@ -37,20 +26,6 @@ defmodule Excelizer.Workbook do
     end
   end
 
-  @spec close_file(Base.file_id()) :: :ok | :error
-  def close_file(file_id), do: Base.close_file(file_id)
-
-  @spec close_file!(Base.file_id()) :: :ok
-  def close_file!(file_id) do
-    case Base.close_file(file_id) do
-      :ok -> :ok
-      :error -> raise Excelizer.Exception, message: "failed to close file"
-    end
-  end
-
-  @spec new_file :: {:ok, Base.file_id()}
-  def new_file, do: Base.new_file()
-
   @spec new_sheet(Base.file_id(), String.t()) :: Base.nif_resp(pos_integer)
   def new_sheet(file_id, sheet_name), do: Base.new_sheet(file_id, sheet_name)
 
@@ -58,6 +33,77 @@ defmodule Excelizer.Workbook do
   def new_sheet!(file_id, sheet_name) do
     case Base.new_sheet(file_id, sheet_name) do
       {:ok, sheet_index} -> sheet_index
+      {:error, err} -> raise Excelizer.Exception, message: err
+    end
+  end
+
+  @spec set_active_sheet(Base.file_id(), pos_integer) :: Base.nif_resp(Base.file_id())
+  def set_active_sheet(file_id, sheet_id), do: Base.set_active_sheet(file_id, sheet_id)
+
+  @spec set_active_sheet!(Base.file_id(), pos_integer) :: Base.file_id()
+  def set_active_sheet!(file_id, sheet_id) do
+    case Base.set_active_sheet(file_id, sheet_id) do
+      {:ok, file_id} -> file_id
+      {:error, err} -> raise Excelizer.Exception, message: err
+    end
+  end
+
+  @spec delete_sheet(Base.file_id(), String.t()) :: Base.nif_resp(Base.file_id())
+  def delete_sheet(file_id, sheet_name), do: Base.delete_sheet(file_id, sheet_name)
+
+  @spec delete_sheet!(Base.file_id(), String.t()) :: Base.file_id()
+  def delete_sheet!(file_id, sheet_name) do
+    case Base.delete_sheet(file_id, sheet_name) do
+      {:ok, file_id} -> file_id
+      {:error, err} -> raise Excelizer.Exception, message: err
+    end
+  end
+
+  @spec copy_sheet(Base.file_id(), pos_integer, pos_integer) :: Base.nif_resp(Base.file_id())
+  def copy_sheet(file_id, from, to), do: Base.copy_sheet(file_id, from, to)
+
+  @spec copy_sheet!(Base.file_id(), pos_integer, pos_integer) :: Base.file_id()
+  def copy_sheet!(file_id, from, to) do
+    case Base.copy_sheet(file_id, from, to) do
+      {:ok, file_id} -> file_id
+      {:error, err} -> raise Excelizer.Exception, message: err
+    end
+  end
+
+  @spec set_sheet_background(Base.file_id(), String.t(), String.t()) ::
+          Base.nif_resp(Base.file_id())
+  def set_sheet_background(file_id, sheet_name, picture_path),
+    do: Base.set_sheet_background(file_id, sheet_name, picture_path)
+
+  @spec set_sheet_background!(Base.file_id(), String.t(), String.t()) :: Base.file_id()
+  def set_sheet_background!(file_id, sheet_name, picture_path) do
+    case Base.set_sheet_background(file_id, sheet_name, picture_path) do
+      {:ok, file_id} -> file_id
+      {:error, err} -> raise Excelizer.Exception, message: err
+    end
+  end
+
+  @spec get_active_sheet_index(Base.file_id()) :: Base.nif_resp(pos_integer)
+  def get_active_sheet_index(file_id),
+    do: Base.get_active_sheet_index(file_id)
+
+  @spec get_active_sheet_index!(Base.file_id()) :: pos_integer()
+  def get_active_sheet_index!(file_id) do
+    case Base.get_active_sheet_index(file_id) do
+      {:ok, sheet_id} -> sheet_id
+      {:error, err} -> raise Excelizer.Exception, message: err
+    end
+  end
+
+  @spec set_active_sheet_visible(Base.file_id(), String.t(), boolean) ::
+          Base.nif_resp(Base.file_id())
+  def set_active_sheet_visible(file_id, sheet_name, visible),
+    do: Base.set_active_sheet_visible(file_id, sheet_name, to_string(visible))
+
+  @spec set_active_sheet_visible!(Base.file_id(), String.t(), boolean) :: Base.file_id()
+  def set_active_sheet_visible!(file_id, sheet_name, visible) do
+    case Base.set_active_sheet_visible(file_id, sheet_name, to_string(visible)) do
+      {:ok, sheet_id} -> sheet_id
       {:error, err} -> raise Excelizer.Exception, message: err
     end
   end
