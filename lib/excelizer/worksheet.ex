2ddab0a0e36d2cd0c26a022c0a757a4197f9ca90
Masashi Yokota <taf_masashi@me.com>
2020-07-12 22:29:58 +0900

    Add set_column_width/5 function to Excelizer.Worksheet module

--- lib/excelizer/worksheet.ex
+++ lib/excelizer/worksheet.ex
@@ -4,6 +4,18 @@ defmodule Excelizer.Worksheet do
   """
   alias Excelizer.Native.Base
 
+  @spec set_col_width(Base.file_id(), String.t(), String.t(), String.t(), number) ::
+          Base.nif_resp(Base.file_id())
   def set_col_width(file_id, sheet_name, start_col, end_col, width) do
+    Base.set_col_width(file_id, sheet_name, start_col, end_col, width)
+  end
+
+  @spec set_col_width!(Base.file_id(), String.t(), String.t(), String.t(), number) ::
+          Base.file_id()
+  def set_col_width!(file_id, sheet_name, start_col, end_col, width) do
+    case Base.set_col_width(file_id, sheet_name, start_col, end_col, width) do
+      {:ok, file_id} -> file_id
+      {:error, err_msg} -> raise Excelizer.Exception, message: err_msg
+    end
   end
 end
---
+++ test/excelizer/worksheet_test.exs
@@ -0,0 +1,50 @@
+defmodule Excelizer.WorksheetTest do
+  use ExUnit.Case
+  alias Excelizer.Native.Base
+  alias Excelizer.Worksheet
+
+  setup do
+    {:ok, file_id} = Base.new_file()
+
+    on_exit(fn ->
+      Base.close_file(file_id)
+    end)
+
+    [file_id: file_id]
+  end
+
+  describe "set_col_width/5" do
+    test "sets width col width", %{file_id: file_id} do
+      {status, resp} = Worksheet.set_col_width(file_id, "Sheet1", "A", "B", 100)
+      assert status == :ok
+      assert resp == file_id
+    end
+
+    test "fails to set col width", %{file_id: file_id} do
+      {status, resp} = Worksheet.set_col_width("invalid ", "Sheet1", "A", "B", 100)
+      assert status == :error
+      assert resp == "given invalid file id"
+
+      {status, resp} = Worksheet.set_col_width(file_id, "Sheet1", "1", "2", 100)
+      assert status == :error
+      assert resp == "invalid column name \"1\""
+    end
+  end
+
+  describe "set_col_width!/5" do
+    test "sets width col width", %{file_id: file_id} do
+      resp = Worksheet.set_col_width!(file_id, "Sheet1", "A", "B", 100)
+      assert resp == file_id
+    end
+
+    test "fails to set col width", %{file_id: file_id} do
+      assert_raise Excelizer.Exception, "given invalid file id", fn ->
+        Worksheet.set_col_width!("invalid ", "Sheet1", "A", "B", 100)
+      end
+
+      assert_raise Excelizer.Exception, "invalid column name \"1\"", fn ->
+        Worksheet.set_col_width!(file_id, "Sheet1", "1", "2", 100)
+      end
+    end
+  end
+end
